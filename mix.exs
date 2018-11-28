62339f285062d6de6c5fd943077b8848ad8e65dd
Vince Forgione <vforgione@uchicago.edu>
2018-11-28 17:11:14 -0600

    lowered minimum version to 1.4

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Socrata.MixProject do
     [
       app: :socrata,
       version: "0.1.0",
-      elixir: "~> 1.7",
+      elixir: "~> 1.4",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
 
